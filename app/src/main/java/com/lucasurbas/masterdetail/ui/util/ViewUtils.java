20f2fa529015984f8d2fb502ca41817eb7fdf8d3
Lucas Urbas <lucas.urbas@gmail.com>
2017-01-28 11:27:52 +0700

    New icon

--- app/src/main/java/com/lucasurbas/masterdetail/ui/util/ViewUtils.java
+++ app/src/main/java/com/lucasurbas/masterdetail/ui/util/ViewUtils.java
@@ -62,7 +62,6 @@ public class ViewUtils {
     public static float convertDpToPixel(float dp, Context context){
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return px;
+        return dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 }
